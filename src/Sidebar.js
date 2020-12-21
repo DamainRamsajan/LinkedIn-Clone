4ca96b1781ccf46a2fbbb9e33bb06d1a2fdd735d
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-21 09:24:34 +0100

    sidebar setup complete

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,16 +3,41 @@ import React from 'react'
 import "./Sidebar.css"
 
 function Sidebar() {
+
+    const recentItem = (topic) => (
+        <div className = "sidebar__recentItem">
+            <span className = "sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
     return (
         <div>
             <div className = "sidebar__top">
-                <img src ="" alt = ""/>
+                <img src ="/images/header-background.jpg" alt = ""/>
                 <Avatar className = "sidebar__avatar" />
                 <h2>Damain Ramsajan</h2>
                 <h4>peterdramsajan@gmail.com</h4>
             </div>
+            <div className = "sidebar__stats">
+                <div className = "sidebar__stat">
+                    <p>Who Viewed You</p>
+                    <p className = "sidebar__statNumber">2777</p>
+                </div>
+                <div className = "sidebar__stat">
+                    <p>Views on Post</p>
+                    <p className = "sidebar__statNumber">3777</p>
+                </div>
+            </div>
 
             <div className = "sidebar__bottom">
+                <p>Recent</p>
+                {recentItem("Dinner With Borat")}
+                {recentItem("Ride to the Moon with Elon")}
+                {recentItem("Klingon War")}
+                {recentItem("Finding Mike Pence Fly")}
+                {recentItem("Cure for Covid")}
+                {recentItem("Next to the cure for blindness")}
 
             </div>
         </div>
