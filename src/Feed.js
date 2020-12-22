43495795151f150229b5af66e5b147eb660ef7d6
Damain Ramsajan <dpramsajan@gmail.com>
2020-12-22 05:55:40 +0100

    Firestore database setup, posts pushed to feed in realtime complete

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Feed.css"
 import CreateIcon from "@material-ui/icons/Create"
 import InputOption from './InputOption'
@@ -7,16 +7,45 @@ import SubscriptionsIcon from "@material-ui/icons/Subscriptions"
 import EventNoteIcon from "@material-ui/icons/EventNote"
 import CalendaViewDayIcon from "@material-ui/icons/CalendarViewDay"
 import Post from './Post'
+import { db } from './firebase'
+import firebase from "firebase"
 
 function Feed() {
+    const [posts, setPosts] = useState ([])
+    const [input, setInput] = useState ("")
+
+    useEffect(() => {
+        db.collection("posts").onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => (
+                {
+                    id: doc.id,
+                    data: doc.data()
+                }
+            )))
+        ))
+        
+    }, [])
+
+    const sendPost = (e) => {
+        e.preventDefault ();
+
+        db.collection("posts").add({
+            name: "Seemore Butts",
+            description: "CEO at Seeing More Butts",
+            message: input,
+            photoUrl: "",
+            timestamp: firebase.firestore.FieldValue.serverTimestamp (),
+
+        })
+    }
     return (
         <div>
            <div className = "feed__inputContainer">
                <div className="feed__input">
                    <CreateIcon />
                    <form>
-                        <input type="text" />
-                        <button type = "submit">Send</button>
+                        <input value = {input} onChange={e => setInput (e.target.value)}  type="text" />
+                        <button onClick={sendPost} type = "submit">Send</button>
                    </form>
                </div>
 
@@ -27,9 +56,21 @@ function Feed() {
                <InputOption Icon = {CalendaViewDayIcon} title ="Write Article" color="#990000"  />
                </div>
            </div>
+          
            <Post name= "Beany Baby" description= "Founder at your moma's place" message= 
            "your mom is still angry with me for rubbing pepper on her butt plug" 
            photoUrl="/images/bean.jpeg" avatar="/images/beany.jpg"/>
+
+           {posts.map(({id, data:{name, description, message, photoUrl, avatar} }) => (
+               <Post
+                    key= {id}
+                    name={name}
+                    description={description}
+                    message={message}
+                    photoUrl={photoUrl}
+                    avatar={avatar}
+               />
+           ))}
            
         </div>
     )
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyAwXWDot-GS6MzV9KUa44nl8q_vYz6BeE4",
     authDomain: "linkedin-clone-d81cb.firebaseapp.com",
@@ -6,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "1088430818431",
     appId: "1:1088430818431:web:9daa06c134dd335dd56b4a"
   };
+
+  const firebaseApp = firebase.initializeApp (firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export {db, auth};
+
+  
